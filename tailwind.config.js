a5dc6274df7810060829e4ee495eee6fbc1d38ab
Michael Murray <mcmurray0125@gmail.com>
2023-01-14 12:54:23 -0600

    initial commit. darkmint theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,19 @@ module.exports = {
     },
     extend: {
       colors: {
-        fabchat: {
+        fabchat: { //darkmint Theme
           primary: 'rgb(33, 72, 220)',
-          background: 'rgba(1, 25, 54, 1)',
-          hoverBackground: '#0A214D',
-          hoverPrimary: '#2B6CB0',
+          background: '#191E29',
+          hoverBackground: '#132D46',
+          hoverPrimary: 'rgba(1, 195, 141, 0.25)',
           hoverSecondary: '#f5f5f5',
+          addServerBtn: 'rgba(1, 195, 141, 1)' ,
           text: '#fafafa',
           white: '#ffffff',
           black: '#000000',
           inputBackground: 'linear-gradient(0deg, rgb(15, 33, 73), rgba(15, 33, 73, 1))',
           topLeft: '#2f3136',
-          subtext: 'rgba(172, 181, 199, 1)',
+          subtext: 'rgba(1, 195, 141, 1)',
         },
       },
       animation: {
