b140d8a5b8baed0427d64038e83b01677bd6be4b
Michael Murray <mcmurray0125@gmail.com>
2023-01-14 17:34:53 -0600

    Dark Mint theme for Fabchat. Added styling for user 'Online' status. Changed placeholder text for message input to be more clear.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,19 +12,32 @@ module.exports = {
     },
     extend: {
       colors: {
-        fabchat: { //darkmint Theme
-          primary: 'rgb(33, 72, 220)',
+        fabchat: { //NEW darkmint Theme
+          primary: '#191E29',
           background: '#191E29',
           hoverBackground: '#132D46',
           hoverPrimary: 'rgba(1, 195, 141, 0.25)',
           hoverSecondary: '#f5f5f5',
-          addServerBtn: 'rgba(1, 195, 141, 1)' ,
+          addServerBtn: 'rgba(1, 195, 141, 1)' , //added color for + button
           text: '#fafafa',
           white: '#ffffff',
           black: '#000000',
           inputBackground: 'linear-gradient(0deg, rgb(15, 33, 73), rgba(15, 33, 73, 1))',
           topLeft: '#2f3136',
           subtext: 'rgba(1, 195, 141, 1)',
+        },
+        fabchatOriginal: { //Original Blue Theme
+          primary: 'rgb(33, 72, 220)',
+          background: 'rgba(1, 25, 54, 1)',
+          hoverBackground: '#0A214D',
+          hoverPrimary: '#2B6CB0',
+          hoverSecondary: '#f5f5f5',
+          text: '#fafafa',
+          white: '#ffffff',
+          black: '#000000',
+          inputBackground: 'linear-gradient(0deg, rgb(15, 33, 73), rgba(15, 33, 73, 1))',
+          topLeft: '#2f3136',
+          subtext: 'rgba(172, 181, 199, 1)',
         },
       },
       animation: {
