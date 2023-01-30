f1c13c97612c933494774dde763beff54665a3b9
Modernbeast02 <89138051+Modernbeast02@users.noreply.github.com>
2023-01-31 00:37:31 +0530

    Update ChannelBar.tsx
    Resolved Issue #17

--- components/common/ChannelBar.tsx
+++ components/common/ChannelBar.tsx
@@ -8,92 +8,94 @@ import { GiSpeaker } from "react-icons/gi";
 import UserBar from "./UserBar";
 
 function ChannelBar() {
-    const router = useRouter();
-    const [server, setServer] = useState("");
-    useEffect(() => {
-        if (!router.isReady) return;
-        setServer(router.query["server"] as string);
-    }, [router.isReady, router.query["server"], router.query["channel"]]);
+  const router = useRouter();
+  const [server, setServer] = useState("");
+  useEffect(() => {
+    if (!router.isReady) return;
+    setServer(router.query["server"] as string);
+  }, [router.isReady, router.query["server"], router.query["channel"]]);
 
-    const [serverDoc] = useDocument(doc(db, "servers", server || "a"));
-    const [channelDocs] = useCollection(
-        collection(db, "servers", server || "a", "channels")
-    );
-    const channels = channelDocs?.docs;
+  const [serverDoc] = useDocument(doc(db, "servers", server || "a"));
+  const [channelDocs] = useCollection(
+    collection(db, "servers", server || "a", "channels")
+  );
+  const channels = channelDocs?.docs;
 
-    const addChannel = () => {
-        const name = prompt("Enter name of channel");
-        addDoc(collection(db, "servers", server || "a", "channels"), {
-            name,
-            type: "text",
-            description: "",
-        }).then(() => {
-            alert("Channel created");
-        });
-    };
+  const addChannel = () => {
+    const name = prompt("Enter name of channel");
+    if (name != "" && name != null) {
+      addDoc(collection(db, "servers", server || "a", "channels"), {
+        name,
+        type: "text",
+        description: "",
+      }).then(() => {
+        alert("Channel created");
+      });
+    }
+  };
 
-    const openSettings = () => {
-        router.push(`/channels/${server}/settings`);
-    };
-    return (
-        <div className="h-full w-full bg-fabchat-background relative">
-            <p className="px-3 py-4 shadow-xl font-bold cursor-pointer hover:bg-fabchat-hoverBackground text-fabchat-text relative">
-                <span onClick={openSettings}>
-                    {serverDoc?.data()?.["name"] as string}
-                </span>
-                <span
-                    onClick={addChannel}
-                    className="absolute right-4 text-2xl my-auto top-3 hover:text-fabchat-primary"
-                >
-                    +
-                </span>
-            </p>
-            <div className="mt-3 px-3 space-y-2">
-                {channels?.map((channel) => {
-                    const data = channel.data();
-                    const props = {
-                        name: data["name"],
-                        type: data["type"],
-                        description: data["description"],
-                        id: channel.id,
-                    };
-                    return <ChannelIcon channel={props} key={props.id} />;
-                })}
-            </div>
+  const openSettings = () => {
+    router.push(`/channels/${server}/settings`);
+  };
+  return (
+    <div className="h-full w-full bg-fabchat-background relative">
+      <p className="px-3 py-4 shadow-xl font-bold cursor-pointer hover:bg-fabchat-hoverBackground text-fabchat-text relative">
+        <span onClick={openSettings}>
+          {serverDoc?.data()?.["name"] as string}
+        </span>
+        <span
+          onClick={addChannel}
+          className="absolute right-4 text-2xl my-auto top-3 hover:text-fabchat-primary"
+        >
+          +
+        </span>
+      </p>
+      <div className="mt-3 px-3 space-y-2">
+        {channels?.map((channel) => {
+          const data = channel.data();
+          const props = {
+            name: data["name"],
+            type: data["type"],
+            description: data["description"],
+            id: channel.id,
+          };
+          return <ChannelIcon channel={props} key={props.id} />;
+        })}
+      </div>
 
-            <UserBar />
-        </div>
-    );
+      <UserBar />
+    </div>
+  );
 }
 
 export default ChannelBar;
 
 function ChannelIcon({ channel }: { channel: Channel }) {
-    const router = useRouter();
-    const [server, setServer] = useState("");
-    useEffect(() => {
-        if (!router.isReady) return;
-        setServer(router.query["server"] as string);
-    }, [router.isReady]);
+  const router = useRouter();
+  const [server, setServer] = useState("");
+  useEffect(() => {
+    if (!router.isReady) return;
+    setServer(router.query["server"] as string);
+  }, [router.isReady]);
 
-    const toggleChannel = () => {
-        router.push(`/channels/${server}/${channel.id}`);
-    };
-    return (
-        <div
-            className="cursor-pointer hover:bg-fabchat-hoverPrimary rounded-lg px-2 py-1"
-            onClick={toggleChannel}
-        >
-            <div className="text-base">
-                {channel.type === "text" ? (
-                    <p className="fomt-semibold flex items-center text-fabchat-text">
-                        <span className="mr-4 font-bold text-lg">#</span>
-                        {channel.name}
-                    </p>
-                ) : (
-                    <GiSpeaker />
-                )}
-            </div>
-        </div>
-    );
+  const toggleChannel = () => {
+    router.push(`/channels/${server}/${channel.id}`);
+  };
+  return (
+    <div
+      className="cursor-pointer hover:bg-fabchat-hoverPrimary rounded-lg px-2 py-1"
+      onClick={toggleChannel}
+    >
+      <div className="text-base">
+        {channel.type === "text" ? (
+          <p className="fomt-semibold flex items-center text-fabchat-text">
+            <span className="mr-4 font-bold text-lg">#</span>
+            {channel.name}
+          </p>
+        ) : (
+          <GiSpeaker />
+        )}
+      </div>
+    </div>
+  );
 }
